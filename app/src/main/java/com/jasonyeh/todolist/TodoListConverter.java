0d4f9f330b0f284e4a380531586ab9d34293473f
Jason Yeh <wadejason80@gmail.com>
2017-08-04 12:03:41 -0700

    implement ListView with viewHolderAdapter which force programmer to use convertView and viewHolder

--- app/src/main/java/com/jasonyeh/todolist/TodoListConverter.java
+++ app/src/main/java/com/jasonyeh/todolist/TodoListConverter.java
@@ -16,17 +16,17 @@ import java.util.List;
  * Created by jasonyeh on 8/3/17.
  */
 
-public class TodoListConverter extends BaseAdapter{
+
+public class TodoListConverter extends ViewHolderAdapter {
 
     private Context context;
     private List<Todo> data;
 
-    public TodoListConverter(@NonNull Context context, @NonNull List<Todo> data) {
+    public TodoListConverter(@NonNull Context context, List<Todo> data) {
         this.context = context;
         this.data = data;
     }
 
-    // list裡面有多少數據
     @Override
     public int getCount() {
         return data.size();
@@ -42,51 +42,103 @@ public class TodoListConverter extends BaseAdapter{
         return position;
     }
 
-    // 數據轉介面
-    // convertView: 介面回收
-    // getView -> UI thread
+    // 實現介面
     @Override
-    public View getView(int position,  View convertView, ViewGroup parent) {
-        ViewHolder vh;
-
-        if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.main_list_item, parent, false);
-
-            vh = new ViewHolder();
-            // we only find once
-            vh.todoText = (TextView) convertView.findViewById(R.id.main_list_item_text);
-            // cache the view holder
-            convertView.setTag(vh);
-        } else {
-            // convertView is not null, which means it contains a cached view holder
-            vh = (ViewHolder) convertView.getTag();
-        }
-
-        Todo todo = data.get(position);
-        vh.todoText.setText(todo.text);
-        return convertView;
-
-//        // findViewById is time consuming
-//        ((TextView) convertView.findViewById(R.id.main_list_item_text)).setText(todo.text);
-//        return convertView;
-
-//        View view = LayoutInflater.from(context).inflate(R.layout.main_list_item, parent ,false);
-//        Todo todo = data.get(position);
-//
-//        ((TextView) view.findViewById(R.id.main_list_item_text)).setText(todo.text);
-//        return view;
+    protected ViewHolder onCreateViewHolder(ViewGroup parent, int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.main_list_item, parent, false);
+        return new TodoListViewHolder(view);
     }
 
-//    public View getView(int position) {
-//        View view = LayoutInflater.from(context).inflate(R.layout.main_list_item, null);
-//        Todo todo = data.get(position);
-//
-//        ((TextView) view.findViewById(R.id.main_list_item_text)).setText(todo.text);
-//        return view;
-//    }
-    private static class ViewHolder {
+    // 實現 數據 and 數據和介面綁定
+    @Override
+    protected void onBindViewHolder(ViewHolder viewHolder, int position) {
+        Todo todo = (Todo) getItem(position);
+        ((TodoListViewHolder) viewHolder).todoText.setText(todo.text);
+    }
+
+    private static class TodoListViewHolder extends ViewHolderAdapter.ViewHolder {
         TextView todoText;
+
+        public TodoListViewHolder(@NonNull View view) {
+            super(view);
+            todoText = (TextView) view.findViewById(R.id.main_list_item_text);
+        }
     }
+
 }
+
+
+//public class TodoListConverter extends BaseAdapter{
+//
+//    private Context context;
+//    private List<Todo> data;
+//
+//    public TodoListConverter(@NonNull Context context, @NonNull List<Todo> data) {
+//        this.context = context;
+//        this.data = data;
+//    }
+//
+//    // list裡面有多少數據
+//    @Override
+//    public int getCount() {
+//        return data.size();
+//    }
+//
+//    @Override
+//    public Object getItem(int position) {
+//        return data.get(position);
+//    }
+//
+//    @Override
+//    public long getItemId(int position) {
+//        return position;
+//    }
+//
+//    // 數據轉介面
+//    // convertView: 介面回收
+//    // getView -> UI thread
+//    @Override
+//    public View getView(int position,  View convertView, ViewGroup parent) {
+//        ViewHolder vh;
+//
+//        if (convertView == null) {
+//            convertView = LayoutInflater.from(context).inflate(R.layout.main_list_item, parent, false);
+//
+//            vh = new ViewHolder();
+//            // we only find once
+//            vh.todoText = (TextView) convertView.findViewById(R.id.main_list_item_text);
+//            // cache the view holder
+//            convertView.setTag(vh);
+//        } else {
+//            // convertView is not null, which means it contains a cached view holder
+//            vh = (ViewHolder) convertView.getTag();
+//        }
+//
+//        Todo todo = data.get(position);
+//        vh.todoText.setText(todo.text);
+//        return convertView;
+//
+////        // findViewById is time consuming
+////        ((TextView) convertView.findViewById(R.id.main_list_item_text)).setText(todo.text);
+////        return convertView;
+//
+////        View view = LayoutInflater.from(context).inflate(R.layout.main_list_item, parent ,false);
+////        Todo todo = data.get(position);
+////
+////        ((TextView) view.findViewById(R.id.main_list_item_text)).setText(todo.text);
+////        return view;
+//    }
+//
+////    public View getView(int position) {
+////        View view = LayoutInflater.from(context).inflate(R.layout.main_list_item, null);
+////        Todo todo = data.get(position);
+////
+////        ((TextView) view.findViewById(R.id.main_list_item_text)).setText(todo.text);
+////        return view;
+////    }
+//    private static class ViewHolder {
+//        TextView todoText;
+//    }
+//}
 
 
